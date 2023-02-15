6bd5de3a9d2590f871709e23bb2add8a5d2b1bd4
Ivan Utkevich <ivanutkevich@gmail.com>
2023-02-15 20:03:46 +0300

    Add project files.

---
+++ BackgroundService/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<FileWatcherService>();
+    })
+    .Build();
+
+host.Run();
+
+public class FileWatcherService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(200, stoppingToken);
+        }
+        await Task.CompletedTask;
+    }
+}
